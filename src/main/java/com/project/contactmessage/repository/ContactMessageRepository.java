76c5722c909bc83cfe145b06305cc221d520208b
seval2016 <sevalsenturk2016@gmail.com>
2024-08-18 16:17:33 +0300

    end poinds added

--- src/main/java/com/project/contactmessage/repository/ContactMessageRepository.java
+++ src/main/java/com/project/contactmessage/repository/ContactMessageRepository.java
@@ -1,7 +1,36 @@
 package com.project.contactmessage.repository;
 
 import com.project.contactmessage.entity.ContactMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ContactMessageRepository  extends JpaRepository<ContactMessage,Long> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
+
+
+    Page<ContactMessage> findByEmailEquals(String email, Pageable pageable);
+
+    Page<ContactMessage> findBySubjectEquals(String subject, Pageable pageable);
+
+    @Query("SELECT c FROM ContactMessage c WHERE FUNCTION('DATE', c.dateTime) BETWEEN ?1 and ?2") //ContactMessage tablosundan istediğim günler arasında tarih bilgileri geliyor.FUNCTION('DATE', c.dateTime) ile dateTime sütununda(javaca field'ında) ki, sadece DATE yani tarih kısmın getirecek. BETWEEN ?1 and ?2 ile de ->  1.parametre ile 2.parametre arasındaki dateleri contactMessage olarak bana getir.
+    List<ContactMessage> findMessagesBetweenDates(LocalDate beginDate, LocalDate endDate);
+
+    @Query("SELECT c FROM ContactMessage c WHERE " +
+            "(EXTRACT(HOUR FROM c.dateTime) BETWEEN :startHour AND :endHour) AND " +
+            "(EXTRACT(HOUR FROM c.dateTime) != :startHour OR EXTRACT(MINUTE FROM c.dateTime) >= :startMinute) AND " + // baslama dakikasina gore kontrol
+            "(EXTRACT(HOUR FROM c.dateTime) != :endHour OR EXTRACT(MINUTE FROM c.dateTime) <= :endMinute)") // bitis dakikasina gore kontrol
+
+    List<ContactMessage> findMessagesBetweenTimes(@Param("startHour") int startHour,
+                                                  @Param("startMinute") int startMinute,
+                                                  @Param("endHour") int endHour,
+                                                  @Param("endMinute") int endMinute);
+
 }
--- src/main/java/com/project/contactmessage/service/ContactMessageService.java
+++ src/main/java/com/project/contactmessage/service/ContactMessageService.java
@@ -4,27 +4,114 @@ import com.project.contactmessage.dto.ContactMessageRequest;
 import com.project.contactmessage.dto.ContactMessageResponse;
 import com.project.contactmessage.entity.ContactMessage;
 import com.project.contactmessage.mapper.ContactMessageMapper;
+import com.project.contactmessage.messages.Messages;
 import com.project.contactmessage.repository.ContactMessageRepository;
+import com.project.exception.ConflictException;
+import com.project.exception.ResourceNotFoundException;
 import com.project.payload.response.business.ResponseMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Objects;
+
+//Service ile anote edilmesi gerekli çünkü bu bir semantik yani business lojic katımız burası developerdan ziyade framework'e de haber vermiş oluyoruz, component yazmış olsaydık bir sorun olmazdı fakat
+//Araştırma Konusu : Kotlin ile entity classlar nasıl yazılır ?
 @Service
 @RequiredArgsConstructor
 public class ContactMessageService {
-
     private final ContactMessageRepository contactMessageRepository;
     private final ContactMessageMapper contactMessageMapper;
 
     public ResponseMessage<ContactMessageResponse> save(ContactMessageRequest contactMessageRequest) {
-            ContactMessage contactMessage =contactMessageMapper.requestToContactMessage(contactMessageRequest);
-        ContactMessage savedData= contactMessageRepository.save(contactMessage);
-        return ResponseMessage.<ContactMessageResponse>builder()
-                .message("Contact Message Created  Successfully")
-                .httpStatus(HttpStatus.CREATED)
-                .object(contactMessageMapper.contactMessageToResponse(savedData))
-                .build();
 
+        //DTO --> POJO
+        ContactMessage contactMessage = contactMessageMapper.requestToContactMessage(contactMessageRequest);
+
+        //yapılan değişikliği db ye kaydediyoruz
+
+        ContactMessage savedData = contactMessageRepository.save(contactMessage);
+
+        return ResponseMessage.<ContactMessageResponse>builder()
+                .message("Contact Message Created Successfully")
+                .httpStatus(HttpStatus.CREATED) // 201
+                .object(contactMessageMapper.contactMessageToResponse(savedData)) //POJO --> DTO
+                .build();
+    }
+
+    public Page<ContactMessageResponse> getAll(int page, int size, String sort, String type) {
+
+        Pageable pageable = PageRequest.of(page,size, Sort.by(sort).ascending());
+        if(Objects.equals(type, "desc")){
+            pageable = PageRequest.of(page,size, Sort.by(sort).descending());
+        }
+
+        //findAll methodundan gelen pageable nesnesi contactMessageMapper aracılığı ile contactMessageToResponse'a dönüştürüldü
+        return contactMessageRepository.findAll(pageable).map(contactMessageMapper::contactMessageToResponse);
+    }
+
+    public Page<ContactMessageResponse> searchByEmail(String email, int page, int size, String sort, String type) {
+
+        Pageable pageable = PageRequest.of(page,size, Sort.by(sort).ascending());
+        if(Objects.equals(type, "desc")){
+            pageable = PageRequest.of(page,size, Sort.by(sort).descending());
+        }
+
+        return contactMessageRepository.findByEmailEquals(email, pageable).
+                map(contactMessageMapper::contactMessageToResponse);
+    }
+
+    public Page<ContactMessageResponse> searchBySubject(String subject, int page, int size, String sort, String type) {
+        Pageable pageable = PageRequest.of(page,size, Sort.by(sort).ascending());
+        if(Objects.equals(type, "desc")){
+            pageable = PageRequest.of(page,size, Sort.by(sort).descending());
+        }
+        return contactMessageRepository.findBySubjectEquals(subject, pageable).
+                map(contactMessageMapper::contactMessageToResponse);// map(contactMessageMapper::contactMessageToResponse) kısmı ile POJO --> DTO'ya cevirir.
+    }
+
+    public String deleteById(Long id) {
+        getContactMessageById(id);
+        contactMessageRepository.deleteById(id);
+        return Messages.CONTACT_MESSAGE_DELETED_SUCCESSFULLY;
+    }
+
+    public ContactMessage getContactMessageById(Long id){
+        return contactMessageRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException(Messages.NOT_FOUND_MESSAGE));
+    }
+
+    public List<ContactMessage> searchBetweenDates(String beginDateString, String endDateString) {
+
+        try {
+            //parametre de gelen String bir değerleri localdate türüne çevirme
+            LocalDate beginDate = LocalDate.parse(beginDateString);
+            LocalDate endDate = LocalDate.parse(endDateString);
+            return contactMessageRepository.findMessagesBetweenDates(beginDate,endDate);
+        } catch (DateTimeParseException e) {
+            throw new ConflictException(Messages.WRONG_DATE_MESSAGE);
+        }
+
+    }
+
+    public List<ContactMessage> searchBetweenTimes(String startHourString, String startMinuteString, String endHourString, String endMinuteString) {
+
+        try {
+            int startHour = Integer.parseInt(startHourString);
+            int startMinute = Integer.parseInt(startMinuteString);
+            int endHour = Integer.parseInt(endHourString);
+            int endMinute = Integer.parseInt(endMinuteString);
+
+            return contactMessageRepository.findMessagesBetweenTimes(startHour,startMinute,endHour,endMinute);
+        } catch (NumberFormatException e) {
+            throw new ConflictException(Messages.WRONG_TIME_MESSAGE);
+        }
     }
 }
